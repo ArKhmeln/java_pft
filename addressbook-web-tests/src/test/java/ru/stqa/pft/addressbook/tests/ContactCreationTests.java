8c908be0a2d517d65013e9bcc97839bb4cfaac4b
Aleksandr Khmelinin <48056732+ArKhmeln@users.noreply.github.com>
2019-05-15 14:37:53 +0300

    Revert "Revert "module 4 for contacts(!need fix)""
    This reverts commit 5ea7c778f0492bacd98337d34343fe6a9a70321e.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -12,9 +14,19 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().createContact(new ContactData("Tester", "Auto",
-                "1234", "mail@test.com", "test1"), true);
+        ContactData contact = new ContactData("Tester", "Auto",
+                "1234", "mail@test.com", "test1");
+        app.getContactHelper().createContact(contact, true);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        //вычисление макс идентификатора не нужно,
+        //contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        //после сортировки сравнение id не нужно
+        Assert.assertEquals(before, after);
     }
 }

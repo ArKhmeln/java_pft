5a0dfc202eebf71a6704ae6a795bcdc7a995c1f3
Aleksandr Khmelinin <48056732+ArKhmeln@users.noreply.github.com>
2019-05-21 18:39:34 +0300

    Hamcrest Groups

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/Groups.java
@@ -0,0 +1,36 @@
+package ru.stqa.pft.addressbook.model;
+
+import com.google.common.collect.ForwardingSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Groups extends ForwardingSet<GroupData> {
+
+    private Set<GroupData> deligate;
+
+    public Groups(Groups groups) {
+        this.deligate = new HashSet<>(groups.deligate);
+    }
+
+    public Groups() {
+        this.deligate = new HashSet<GroupData>();
+    }
+
+    @Override
+    protected Set<GroupData> delegate() {
+        return deligate;
+    }
+
+    public Groups withAdded(GroupData group) {
+        Groups groups = new Groups(this);
+        groups.add(group);  //add работает т.к. реализован в ForwardingSet
+        return groups;
+    }
+
+    public Groups without(GroupData group) {  //делает копию, из которой удалена группа
+        Groups groups = new Groups(this);
+        groups.remove(group);  //add работает т.к. реализован в ForwardingSet
+        return groups;
+    }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,30 +1,36 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.goTo().GroupPage();
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData group = new GroupData().withName("test1");
         app.group().create(group);
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() + 1); //кол-во групп до должно быть = кол-ву гр. после
+        Groups after = app.group().all();
+        assertThat(after.size(), equalTo(before.size() + 1)); //кол-во групп до должно быть = кол-ву гр. после
 
         //как найти идентификатор новой группы? В новой группе id максимальный, сравниваем друг с другом
         //список в поток, ф-я для мах эл-та (сравн. объекты GroupDara с пом. сравнения их Id). на выходе мах объект
         //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());    //присвоить новой добавленной группе правильный id
-        before.add(group);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));   //присвоить новой добавленной группе правильный id
     }
 }
 

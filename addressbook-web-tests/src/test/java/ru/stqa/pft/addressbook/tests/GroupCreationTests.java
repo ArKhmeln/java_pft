455d113120c943c3b8988d1490eb7460e2ee6c54
Aleksandr Khmelinin <48056732+ArKhmeln@users.noreply.github.com>
2019-05-23 15:39:36 +0300

    hashing (Groups) l55

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,15 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
-
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -23,14 +16,31 @@ public class GroupCreationTests extends TestBase {
         Groups before = app.group().all();
         GroupData group = new GroupData().withName("test1");
         app.group().create(group);
+        assertThat((app.group().count()), equalTo(before.size() + 1));
         Groups after = app.group().all();
-        assertThat(after.size(), equalTo(before.size() + 1)); //кол-во групп до должно быть = кол-ву гр. после
+         //кол-во групп до должно быть = кол-ву гр. после
 
         //как найти идентификатор новой группы? В новой группе id максимальный, сравниваем друг с другом
         //список в поток, ф-я для мах эл-та (сравн. объекты GroupDara с пом. сравнения их Id). на выходе мах объект
         //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));   //присвоить новой добавленной группе правильный id
+    }
+
+    @Test   //негантивный тест, группа с апострофом' не доджна создаваться
+    public void testBadGroupCreation() {
+        app.goTo().GroupPage();
+        Groups before = app.group().all();
+        GroupData group = new GroupData().withName("test1'");
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()));    //предварительная проверка с пом-ю быстрой операции
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before));
+
+        //как найти идентификатор новой группы? В новой группе id максимальный, сравниваем друг с другом
+        //список в поток, ф-я для мах эл-та (сравн. объекты GroupDara с пом. сравнения их Id). на выходе мах объект
+        //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        assertThat(after, equalTo(before));
     }
 }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -31,6 +31,7 @@ public class GroupModificationTests extends TestBase {
         GroupData group = new GroupData()
                 .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
         app.group().modify(group);  //объект последовательно модифицируется, шаблон Builder
+        assertThat((app.group().count()), equalTo(before.size()));
         Groups after = app.group().all();
         assertEquals(after.size(), before.size());
         assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
